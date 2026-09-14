d7bc0304b01abfed572569c136cdc4d8465d899c
Sithmaka Nanayakkara <IT22103918@my.sliit.lk>
2026-09-15 00:40:40 +0530

    feat: Add Constants object for configuration settings including BASE_URL, network timeout, and database details

---
+++ mobile/app/src/main/java/com/smartsolar/mobile/utils/Constants.kt
@@ -0,0 +1,17 @@
+package com.smartsolar.mobile.utils
+
+object Constants {
+    /**
+     * Base URL for connecting to the ASP.NET Core backend.
+     * Note: In the Android Emulator, "10.0.2.2" refers to the host machine's "localhost".
+     * If testing on a physical device over Wi-Fi, change this to your machine's LAN IP.
+     */
+    const val BASE_URL = "http://10.0.2.2:5295/"
+
+    // Request Timeout
+    const val NETWORK_TIMEOUT_SECONDS = 30L
+
+    // Database
+    const val DATABASE_NAME = "smart_solar_local.db"
+    const val DATABASE_VERSION = 1
+}
